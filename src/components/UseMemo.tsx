9bf581cc3db699af196740a5f49373435cfca4f4
Ina Davidova <ina.davidova@envistaco.com>
2024-01-27 10:02:12 +0100

    Added useMemo example

--- src/components/UseMemo.tsx
+++ src/components/UseMemo.tsx
@@ -1,9 +1,38 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 
 const UseMemoExample = () => {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [sum, setSum] = useState(0);
+
+  const someHeavyCalculation = useMemo(() => {
+    console.log("doing heavy calculations");
+
+    const result = number1 + number2;
+    // setSum(result);
+    return result;
+  },[number1, number2]);
+
   return (
     <>
       <h2>useMemo</h2>
+      <input
+        type="number"
+        value={number1}
+        onChange={(e) => setNumber1(Number(e.target.value))}
+      />
+
+      <span> + </span>
+
+      <input
+        type="number"
+        value={number2}
+        onChange={(e) => setNumber2(Number(e.target.value))}
+      />
+
+      <button onClick={() => setSum(someHeavyCalculation)}>Calculate</button>
+
+      <p>The sum of this equation in {sum}</p>
     </>
   );
 };
